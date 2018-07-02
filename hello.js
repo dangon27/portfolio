81f5cc655f11e3e48683777bce201f1406d670b8
Daniel Gonzalez <gonzaled@lafayette.edu>
2018-07-02 18:51:15 -0400

    s

---
+++ hello.js
@@ -0,0 +1,11 @@
+new Vue ({
+    el: '#one',
+    data: {
+        title: 'blah',
+        subtitle: 'blah',
+        bio: 'blah',
+        quote: 'blah',
+        subtitle2: 'blah'
+    }
+    });
+    
---
+++ index.js
@@ -0,0 +1,8 @@
+new Vue ({
+    el: '#one',
+    data: {
+        try: 'did it work?',
+        title: 'Look at where I have been...',
+        subtitle1: 'lets go'
+    }
+    });
