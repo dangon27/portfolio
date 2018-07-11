adc04bca47286bf95982b715698b57df68c492af
Daniel Gonzalez <gonzaled@lafayette.edu>
2018-07-11 22:14:00 +0200

    vueify

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-new Vue ({
+var first= new Vue ({
     el: '#one',
     data: {
         title: 'blah',
@@ -8,4 +8,24 @@ new Vue ({
         subtitle2: 'blah'
     }
     });
+
+var second= new Vue ({
+    el: '#signup-form',
+    data: {
+        name: 'ASDF',
+        email: 'ASDF',
+        subject:'ASDF',
+        message:'ASDF'
+    },
+
+    methods: {
+        processForm: function() {
+            console.log({name: this.name, email: this.email, subject: this.subject, message: this.message});
+            alert('Processing!');
+        }
+    }
+
+
+
+})
     
